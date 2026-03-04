44cfb47a60f4e044072da67ad47dc7aa1d4ade72
Neil Couture - Tachyon <neil@symetryml.net>
2026-03-03 23:26:36 -0500

    change default host to localhost

--- src/otelfl/config.py
+++ src/otelfl/config.py
@@ -10,10 +10,10 @@ from pathlib import Path
 @dataclass
 class Settings:
     flagd_url: str = field(
-        default_factory=lambda: os.environ.get("OTELFL_FLAGD_URL", "http://10.0.0.5:8080/feature")
+        default_factory=lambda: os.environ.get("OTELFL_FLAGD_URL", "http://localhost:8080/feature")
     )
     locust_url: str = field(
-        default_factory=lambda: os.environ.get("OTELFL_LOCUST_URL", "http://10.0.0.5:8080/loadgen/")
+        default_factory=lambda: os.environ.get("OTELFL_LOCUST_URL", "http://localhost:8080/loadgen/")
     )
     poll_interval: float = field(
         default_factory=lambda: float(os.environ.get("OTELFL_POLL_INTERVAL", "2.0"))
